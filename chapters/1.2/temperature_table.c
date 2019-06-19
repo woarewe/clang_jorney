3244a5f84d232dbbacf1b17b08fd9ee395b38d33
woarewe <rostislav.zhuravsky@gmail.com>
2019-06-20 02:19:39 +0300

    Make temperature table program

---
+++ chapters/1.2/temperature_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int fahrenheit_to_celsius(int *fahrenheit_pointer) {
+    return 5 * (*fahrenheit_pointer - 32) / 9;
+}
+
+int main() {
+    int fahrenheit, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahrenheit = lower;
+    while(fahrenheit <= upper) {
+        printf("%d\t%d\n", fahrenheit, fahrenheit_to_celsius(&fahrenheit));
+        fahrenheit += step;
+    }
+
+    return 0;
+}
