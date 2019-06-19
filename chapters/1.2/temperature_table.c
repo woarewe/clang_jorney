7b3ab8295767ad7b6ee2f0a6c0190426aca036a3
woarewe <rostislav.zhuravsky@gmail.com>
2019-06-20 02:57:19 +0300

    Imporve code quality via using #define directive

--- chapters/1.2/temperature_table.c
+++ chapters/1.2/temperature_table.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LOWER 0.0
+#define UPPER 300.0
+#define STEP 20.0
+
 float fahrenheit_to_celsius(float *fahrenheit_pointer) {
     return (5.0 / 9.0) * (*fahrenheit_pointer - 32.0);
 }
@@ -22,7 +26,7 @@ int main() {
     printf("|%10s | %10s|\n", "Celsius", "Fahrenheit");
     printf("|%s|\n", delimiter_string);
 
-    for (float fahrenheit = 300.0; fahrenheit >= 0.0; fahrenheit = fahrenheit - 20.0) {
+    for (float fahrenheit = UPPER; fahrenheit >= LOWER; fahrenheit = fahrenheit - STEP) {
         printf("|%10.0f | %10.1f|\n", fahrenheit, fahrenheit_to_celsius(&fahrenheit));
         printf("|%s|\n", delimiter_string);
     }
