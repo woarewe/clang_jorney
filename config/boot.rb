b22555925828c114537d84acc68288b1248899e5
woarewe <rostislav.zhuravsky@gmail.com>
2019-10-22 16:26:56 +0300

    Improve application boot

--- config/boot.rb
+++ config/boot.rb
@@ -2,23 +2,26 @@
 
 require 'bundler/setup'
 
-ZEITWERK_DIRS = %w[lib].freeze
+ZEITWERK_DIRS = [
+  File.expand_path('../lib', __dir__)
+].freeze
+
+INFLECTION_MAPPING = {
+  cli: :CLI
+}.freeze
 
 Bundler.require(:default)
 
 inflector_class = Class.new(Zeitwerk::Inflector) do
   def camelize(basename, _absolute_path)
-    case basename
-    when 'cli'
-      'CLI'
-    else
-      super
-    end
+    INFLECTION_MAPPING.fetch(basename.to_sym, super)
   end
 end
 
 loader = Zeitwerk::Loader.new
 loader.inflector = inflector_class.new
-ZEITWERK_DIRS.each { |dir| loader.push_dir(dir) }
+
+ZEITWERK_DIRS.each(&loader.method(:push_dir))
+
 loader.setup
 loader.eager_load
